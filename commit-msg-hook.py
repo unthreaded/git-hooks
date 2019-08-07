528f04f7b69bff89558e9343693084bf7fa3e813
tiemonl <tiemon.liam@gmail.com>
2019-08-06 21:48:19 -0400

    GH-2: added constants

--- commit-msg-hook.py
+++ commit-msg-hook.py
@@ -1,9 +1,13 @@
-import os
 from pygit2 import Repository
+from git import Repo
 
 def commit_msg():
-    branch = Repository('.').head.shorthand
-    branchNoDir = branch.rsplit("/",1)[-1]
-    print(branchNoDir)
+    BRANCH = Repository('.').head.shorthand
+    BRANCH_NO_DIR = BRANCH.rsplit("/",1)[-1]
+    ISSUE_PATTERN="GH-[0-9]+"
+    TICKET_BASE_URL="https://github.com/unthreaded/issues/"
+    NO_TICKET_KEYWORD="NOGH"
+
+    #TODO: add logic for commit-msg
 
 commit_msg()
