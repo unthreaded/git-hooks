ec3c1b90811d9bfdf1c79be06cb0339465c3a227
Lucas Combs <l-combs@hotmail.com>
2019-08-28 19:36:39 -0400

    GH-10 | Added failing unit test to verify build will fail

--- src/test/test_commit_msg_hook.py
+++ src/test/test_commit_msg_hook.py
@@ -9,6 +9,9 @@ class TestCommitHook(unittest.TestCase):
     def test_dummy_starter_2(self):
         self.assertFalse(False)
 
+    def test_dummy_failing_test(self):
+        self.assertFalse(True)
+
 
 if __name__ == '__main__':
     unittest.main()
