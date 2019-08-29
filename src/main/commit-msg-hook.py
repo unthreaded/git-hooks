449c4a768f37b934f41cfb0d8d884357a6e0d291
Lucas Combs <l-combs@hotmail.com>
2019-08-28 20:25:42 -0400

    GH-10 | Remove failing unit test and mend lint errors

--- src/main/commit-msg-hook.py
+++ src/main/commit-msg-hook.py
@@ -1,3 +1,6 @@
+"""
+    This file handles finding a ticket number and placing it into the commit message
+"""
 from __future__ import print_function
 
 import logging
@@ -5,46 +8,51 @@ import os
 import re
 import sys
 
+BRANCH = sys.argv[1]
+COMMIT_MSG = sys.argv[2]
+GH_ISSUE_URL = sys.argv[3]
+EXEMPTION_KEYWORDS = "^Merge|^Revert"
+PROTECTED_BRANCHES = "^dev|^release.*|^hotfix.*"
+BRANCH_NO_DIR = BRANCH.rsplit("/", 1)[-1]
+ISSUE_PATTERN = "GH-[0-9]+|NOGH"
+NO_TICKET_KEYWORD = "NOGH"
+
 
 def commit_msg():
-    BRANCH = sys.argv[1]
-    COMMIT_MSG = sys.argv[2]
-    GH_ISSUE_URL = sys.argv[3]
-    EXEMPTION_KEYWORDS = "^Merge|^Revert"
-    PROTECTED_BRANCHES = "^dev|^release.*|^hotfix.*"
-    BRANCH_NO_DIR = BRANCH.rsplit("/", 1)[-1]
-    ISSUE_PATTERN = "GH-[0-9]+|NOGH"
-    NO_TICKET_KEYWORD = "NOGH"
-
+    """
+        Add the ticket to the git commit message if possible
+    """
     issue = NO_TICKET_KEYWORD
-    isCommitCompliant = True
-    isBranchNonCompliant = False
+    is_commit_compliant = True
+    is_branch_non_compliant = False
 
     if any(re.findall(EXEMPTION_KEYWORDS, COMMIT_MSG, re.IGNORECASE)):
         logging.info("Merging or Reverting, will not change commit message.")
         exit(0)
 
     if any(re.findall(PROTECTED_BRANCHES, BRANCH, re.IGNORECASE)):
-        logging.error("You just committed to an exempt branch! " + BRANCH)
+        logging.error("You just committed to an exempt branch! ( %s )", BRANCH)
         exit(1)
 
     commit_pattern = ISSUE_PATTERN + ": .*"
     if not any(re.findall(commit_pattern, COMMIT_MSG)):
-        isCommitCompliant = False
+        is_commit_compliant = False
 
     if not any(re.findall(ISSUE_PATTERN, BRANCH_NO_DIR)):
-        isBranchNonCompliant = True
+        is_branch_non_compliant = True
 
-    if isBranchNonCompliant and (not isCommitCompliant):
-        logging.info("Cannot find ticket in branch name, assuming NOGH: " + BRANCH)
+    if is_branch_non_compliant and (not is_commit_compliant):
+        logging.info("Cannot find ticket in branch name, assuming NOGH: %s", BRANCH)
     else:
         issue = re.findall(ISSUE_PATTERN, BRANCH_NO_DIR)[0]
 
-    if isCommitCompliant:
+    if is_commit_compliant:
         commit_issue = re.findall(ISSUE_PATTERN, COMMIT_MSG)[0]
         if issue != commit_issue:
-            logging.info("GH issue in commit does not match branch (%s != %s), will not rewrite commit."
-                         % (commit_issue, issue))
+            logging.info(
+                "GH issue in commit does not match branch (%s != %s), will not rewrite commit.",
+                commit_issue,
+                issue)
             exit(0)
         exit(0)
 
@@ -52,17 +60,14 @@ def commit_msg():
     final_commit_msg = "%s: %s" % (issue, COMMIT_MSG)
     if issue != NO_TICKET_KEYWORD:
         issue = "%s%s" % (GH_ISSUE_URL, issue_num)
-    logging.info("Rewriting commit to use issue: %s" % issue)
+    logging.info("Rewriting commit to use issue: %s", issue)
 
-    p = os.path.join('.git', 'COMMIT_EDITMSG')
-    with open(p, "w") as f:
-        f.write(final_commit_msg)
+    commit_msg_file_path = os.path.join('.git', 'COMMIT_EDITMSG')
+    with open(commit_msg_file_path, "w") as commit_file:
+        commit_file.write(final_commit_msg)
     exit(0)
 
 
-def main():
+if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
     commit_msg()
-
-
-main()
--- src/test/test_commit_msg_hook.py
+++ src/test/test_commit_msg_hook.py
@@ -9,9 +9,6 @@ class TestCommitHook(unittest.TestCase):
     def test_dummy_starter_2(self):
         self.assertFalse(False)
 
-    def test_dummy_failing_test(self):
-        self.assertFalse(True)
-
 
 if __name__ == '__main__':
     unittest.main()
