9520212f623c07c0a616cb72b4f8546350d4a6a1
Lucas Combs <l-combs@hotmail.com>
2019-10-05 17:40:34 -0400

    GH-14: PyInstaller executables are not generated to: dist/<OS NAME>/commit_msg
    rather than
    
    dist/commit_msg_<OS NAME>.exe

--- scripts/package.py
+++ scripts/package.py
@@ -11,10 +11,18 @@ OS_PREFIX: str = platform.system().lower()
 if OS_PREFIX == "darwin":
     OS_PREFIX = "mac"
 
+EXE_NAME: str = "commit_msg"
+EXE_FILE_FOLDER = os.path.join('dist', OS_PREFIX)
+
 # Hidden import mends PyInstaller moduleNotFound errors
 PyInstaller.__main__.run([
     "--onefile",
     "--hidden-import=_cffi_backend",
-    "--name=%s" % "commit_msg_%s.exe" % OS_PREFIX,
+    "--distpath=%s" % EXE_FILE_FOLDER,
+    "--name=%s" % EXE_NAME,
     os.path.join("src", "main", "commit_msg_hook.py"),
 ])
+
+# Remove file extension from executable
+FINAL_EXE_PATH = os.path.join(EXE_FILE_FOLDER, EXE_NAME)
+os.rename(FINAL_EXE_PATH + ".exe", FINAL_EXE_PATH)
