fa836c9197859db85e944745a683729d16965bc1
Lucas Combs <l-combs@hotmail.com>
2019-10-06 12:43:04 -0400

    GH-25: Setup local debugging and tests

--- src/test/test_commit_msg_hook.py
+++ src/test/test_commit_msg_hook.py
@@ -1,22 +1,49 @@
-import unittest
-from unittest.mock import patch, Mock
-from src.main import commit_msg_hook
+from unittest.mock import patch, Mock, ANY
+
+from src.main import commit_msg_hook as sut
+from src.main.hook.commit_msg_hook_runner import ExitCode
+from src.test.base_unit_test import BaseUnitTest
 
 
-class TestCommitMsgHook(unittest.TestCase):
-    def test_incorrect_command_line_args(self):
-        """
-            When we receive the incorrect number of command line arguments,
-             we should exit with FAILURE
-        """
+# Main will not run without the correct __name__
+@patch("src.main.commit_msg_hook.__name__", "__main__")
+class TestCommitMsgHook(BaseUnitTest.BaseTestCase):
+    mock_hook_runner: Mock
+    mock_sys: Mock
+    mock_os: Mock
+    SUT_PATCH: str = "src.main.commit_msg_hook"
 
+    def setUp(self):
+        self.mock_hook_runner = self.create_patch(self.SUT_PATCH + ".CommitMessageHookRunner")
+        # Default return code SUCCESS
+        self.mock_hook_runner.return_value.run.return_value = ExitCode.SUCCESS
+        self.mock_sys = self.create_patch(self.SUT_PATCH + ".sys")
+        self.mock_os = self.create_patch(self.SUT_PATCH + ".os")
+
+    def test_incorrect_prod_command_line_args(self):
         # Setup an array with one too many arguments
-        mock_args = ["arg"] * (commit_msg_hook.NUM_ARGUMENTS_EXPECTED + 1)
+        self.mock_sys.argv = ["arg"] * (sut.NUM_PROD_ARGUMENTS_EXPECTED + 1)
+        sut.main()
+        self.mock_sys.exit.assert_called_once_with(ExitCode.FAILURE.value)
 
-        with patch("sys.argv", mock_args):
-            with patch.object(commit_msg_hook, "__name__", "__main__"):
-                mock_exit = Mock(name="exit")
-                commit_msg_hook.sys.exit = mock_exit
-                commit_msg_hook.main()
+    def test_incorrect_debug_command_line_args(self):
+        self.mock_sys.argv = ['Dummy path', sut.DEBUG_FLAG]
+        sut.main()
+        self.mock_sys.exit.assert_called_once_with(ExitCode.FAILURE.value)
 
-                mock_exit.assert_called_once_with(commit_msg_hook.ExitCode.FAILURE.value)
+    def test_correct_debug_command_line_args(self):
+        self.mock_sys.argv = ['Dummy path', sut.DEBUG_FLAG, "X", "Y"]
+        sut.main()
+
+        # Should exit success
+        self.mock_sys.exit.assert_called_once_with(ExitCode.SUCCESS.value)
+        self.mock_hook_runner.assert_called_once_with("X", "Y", ANY)
+
+    def test_correct_prod_command_line_args(self):
+        self.mock_os.getcwd.return_value = "test_value"
+        self.mock_sys.argv = ['Dummy path', "VALUE"]
+        sut.main()
+
+        # Should exit success
+        self.mock_sys.exit.assert_called_once_with(ExitCode.SUCCESS.value)
+        self.mock_hook_runner.assert_called_once_with("test_value", "VALUE", ANY)
