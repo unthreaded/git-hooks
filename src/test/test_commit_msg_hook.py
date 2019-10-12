ed63ec1253a9f6a522f3b5439c4fb30638a65005
Lucas Combs <l-combs@hotmail.com>
2019-10-12 15:45:49 -0400

    GH-25: Added tests for get_config

--- src/test/test_commit_msg_hook.py
+++ src/test/test_commit_msg_hook.py
@@ -1,6 +1,7 @@
-from unittest.mock import patch, Mock, ANY
+from unittest.mock import patch, Mock, ANY, MagicMock
 
 from src.main import commit_msg_hook as sut
+from src.main.config.commit_hook_config import CommitHookConfig
 from src.main.hook.commit_msg_hook_runner import ExitCode
 from src.test.base_unit_test import BaseUnitTest
 
@@ -11,6 +12,9 @@ class TestCommitMsgHook(BaseUnitTest.BaseTestCase):
     mock_hook_runner: Mock
     mock_sys: Mock
     mock_os: Mock
+    mock_yaml_config: Mock
+    mock_default_config: Mock
+    mock_logging: Mock
     SUT_PATCH: str = "src.main.commit_msg_hook"
 
     def setUp(self):
@@ -20,7 +24,20 @@ class TestCommitMsgHook(BaseUnitTest.BaseTestCase):
         self.mock_sys = self.create_patch(self.SUT_PATCH + ".sys")
         self.mock_os = self.create_patch(self.SUT_PATCH + ".os")
 
-        self.create_patch(self.SUT_PATCH + ".CommitHookConfigYAMLImpl")
+        # When a YAML config implementation is constructed, return our mock
+        self.mock_yaml_config = MagicMock(spec=CommitHookConfig)
+        self.create_patch(
+            self.SUT_PATCH + ".CommitHookConfigYAMLImpl"
+        ).return_value = self.mock_yaml_config
+
+        # When a default config implementation is constructed, return our mock
+        self.mock_default_config = MagicMock(spec=CommitHookConfig)
+        self.create_patch(
+            self.SUT_PATCH + ".CommitHookConfigDefaultImpl"
+        ).return_value = self.mock_default_config
+
+        self.mock_logging = self.create_patch(self.SUT_PATCH + ".logging")
+
         self.create_patch('builtins.open')
 
     def test_incorrect_prod_command_line_args(self):
@@ -52,3 +69,23 @@ class TestCommitMsgHook(BaseUnitTest.BaseTestCase):
         sut.main()
         self.mock_sys.exit.assert_called_once_with(ExitCode.FAILURE.value)
         self.mock_hook_runner.assert_not_called()
+
+    def test_get_config_with_yaml_config_file(self):
+        self.mock_os.path.isfile.return_value = True
+        self.assertEqual(
+            sut.get_config(),
+            self.mock_yaml_config,
+            "A YAML implementation was not returned")
+
+    def test_get_config_without_yaml_config_file(self):
+        self.mock_os.path.isfile.return_value = False
+
+        self.assertEqual(
+            sut.get_config(),
+            self.mock_default_config,
+            "A default implementation was not returned")
+
+        self.assertEqual(
+            1,
+            len(self.mock_logging.method_calls),
+            "A warning should get logged to the console")
