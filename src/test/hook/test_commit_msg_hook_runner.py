e4c25e15eaac8feba463c8e9740dbec5512956b8
Lucas Combs <l-combs@hotmail.com>
2019-10-17 18:54:14 -0400

    GH-53: tested unborn/detached head

--- src/test/hook/test_commit_msg_hook_runner.py
+++ src/test/hook/test_commit_msg_hook_runner.py
@@ -17,6 +17,12 @@ class TestCommitMessageRunner(BaseUnitTest.BaseTestCase):
     mock_logging: Mock
     SUT_PATCH: str = "src.main.hook.commit_msg_hook_runner"
 
+    def set_detached_head_flag(self, boolean: bool):
+        self.mock_repo.return_value.head_is_detached = boolean
+
+    def set_unborn_head_flag(self, boolean: bool):
+        self.mock_repo.return_value.head_is_unborn = boolean
+
     def setUp(self):
         self.sut = CommitMessageHookRunner(self.repo_path, self.commit_msg_path, self.config)
         self.message_written_to_commit_file = None
@@ -31,9 +37,9 @@ class TestCommitMessageRunner(BaseUnitTest.BaseTestCase):
         self.mock_open = self.create_patch('builtins.open')
         self.mock_open.return_value = self.mock_commit_file
         self.mock_repo = self.create_patch(self.SUT_PATCH + ".Repository")
-        self.mock_repo.return_value.head_is_unborn = False
-        self.mock_repo.return_value.head_is_detached = False
         self.mock_logging = self.create_patch(self.SUT_PATCH + ".logging")
+        self.set_unborn_head_flag(False)
+        self.set_detached_head_flag(False)
 
         # Setup config information
         self.config.get_issue_pattern.return_value = "TICKET-[0-9]+"
@@ -142,3 +148,12 @@ class TestCommitMessageRunner(BaseUnitTest.BaseTestCase):
                           "Nothing should have been written to commit file")
 
         self.assert_n_calls_made_to_logging(1)
+
+    def test_detached_head_returns_empty_branch(self):
+        self.set_detached_head_flag(True)
+        self.assertEqual("", self.sut.get_current_branch_name())
+
+    def test_unborn_head_returns_empty_branch(self):
+        self.set_unborn_head_flag(True)
+        self.assertEqual("", self.sut.get_current_branch_name())
+
